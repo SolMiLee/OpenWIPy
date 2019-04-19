1857eb11c9e2ae594539e5a358c07e326066c80e
yoga-sangggho <sangggho@gmail.com>
2019-04-19 17:55:59 +0900

    add code

--- openwipy.py
+++ openwipy.py
@@ -3,29 +3,37 @@ import pandas as pd
 import numpy as np
 
 
+# data modify
 def read_csv_weld_data(path=str, col_nm=None):
     raw_data = pd.read_csv(path, header=None, names=col_nm, index_col=False)
     raw_data_cleaning = raw_data.dropna(axis=1)
     return raw_data_cleaning
 
 
-def plot_current_voltage(raw_data):
-    pass_time = raw_data['Time']
-    pass_current = raw_data['Current']
-    pass_voltage = raw_data['Voltage']
+def read_xl_weld_data(path=str):
+    pass
+
+
+def specify_weld_data_interval(data, step=None):
+    step_data = data.iloc[::step]
+    return step_data
+
+
+# plot data
+def plot_current_voltage(col_time, col_current, col_voltage):
+    pass_time = col_time
+    pass_current = col_current
+    pass_voltage = col_voltage
 
     fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(15, 6))
     ax[0].plot(pass_time, pass_current, linewidth=0.5)
-    ax[0].set_ylabel('Current (A)', fontsize=20)
+    ax[0].set_title('Current, Voltage Signal')
+    ax[0].set_ylabel('Current (A)', fontsize=15)
     ax[1].plot(pass_time, pass_voltage, 'k', linewidth=0.5)
-    ax[1].set_xlabel('Time (sec', fontsize=20)
-    ax[1].set_ylabel('Voltage (V)', fontsize=20)
+    ax[1].set_xlabel('Time (sec)', fontsize=15)
+    ax[1].set_ylabel('Voltage (V)', fontsize=15)
 
     for x in ax:
-        x.tick_params(labelsize=15)
+        x.tick_params(labelsize=10)
 
     return plt.show()
-
-# ReadCSVData('C:\Prjt\OpenWIPy\data\S01P04A.csv')
-# rdata = ReadCSVData('C:\Prjt\OpenWIPy\data\S01P04A.csv')
-# PlotCV(rdata)
